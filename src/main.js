cf98ac9fb0d81d609782b17f6a569c36512c730a
alexlabuz <alex.labuz@live.fr>
2022-10-13 23:07:02 +0200

    Update router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue'
 import HomePage from './pages/HomePage.vue'
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
 })
 
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/notes/",
+  base: process.env.NODE_ENV === "production" ? "/notes/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
