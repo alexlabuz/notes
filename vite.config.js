41839b707ea9da221b3ac445cfe7996c3f3bae63
alexlabuz <alex.labuz@live.fr>
2022-10-13 23:11:59 +0200

    publish

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/notes/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
